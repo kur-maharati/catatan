2eb4426be6201b6ed1fb39da2e44bf2d50d3ac6b
kur-maharati <kur.maharati@gmail.com>
2026-08-03 13:17:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
 
-    API_URL: "https://script.google.com/macros/s/AKfycbzgwEVb46nJhtOxSBrdrdiG3OwCMqPqLxwz3fYd_OuPIVrzHn2iaB1fJGEN8zQu4e4pkg/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycbyZ6PYYJD1-Rrd8JrL17y3Wws5FWxGqKoNutAtvFuy7YM5EiBhaPeNMdvJ_46qI4xC2-g/exec",
 
     REFRESH_INTERVAL: 30000,
 
